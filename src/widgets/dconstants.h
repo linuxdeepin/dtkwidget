be574c6eccc618e481d9471002dac17f1f7ff3e9
Iceyer <me@iceyer.net>
2017-09-12 15:34:07 +0800

    Update license
    Change-Id: I10eb13e6764fe268eefcae555f0546c2fb8e4b49

--- src/widgets/dconstants.h
+++ src/widgets/dconstants.h
@@ -1,20 +1,20 @@
-﻿/**
- * Copyright (C) 2015 Deepin Technology Co., Ltd.
+/*
+ * Copyright (C) 2015 ~ 2017 Deepin Technology Co., Ltd.
  *
- * This program is free software; you can redistribute it and/or modify
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.
- **/
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
-/*************************************************************
-*File Name: dconstants.h
-*Author: Match
-*Email: Match.YangWanQing@gmail.com
-*Created Time: 2015年07月27日 星期一 15时16分54秒
-*Description:
-*
-*************************************************************/
 #ifndef DCONSTANTS_H
 #define DCONSTANTS_H
 
