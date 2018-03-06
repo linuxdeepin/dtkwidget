a926ba930d175d94d0aae7fc226a8ba51158ed1d
Iceyer <me@iceyer.net>
2018-03-06 13:56:56 +0800

    update translate
    Change-Id: I8cde148534a2f1246ef64a3de39849774324d5d7

--- translations/dtkwidget2.ts
+++ translations/dtkwidget2.ts
@@ -2,88 +2,30 @@
 <!DOCTYPE TS>
 <TS version="2.1">
 <context>
-    <name>DAboutDialog</name>
+    <name>CameraForm</name>
     <message>
-        <location filename="../src/widgets/daboutdialog.cpp" line="136"/>
-        <source>Acknowledgements</source>
+        <location filename="../examples/dwidget-examples/collections/cameraform.ui" line="14"/>
+        <source>Form</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/widgets/dapplication.cpp" line="551"/>
-        <source>Version: %1</source>
+        <location filename="../examples/dwidget-examples/collections/cameraform.ui" line="52"/>
+        <source>ok</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/widgets/dapplication.cpp" line="555"/>
-        <source>%1 is released under %2</source>
-        <translation type="unfinished"></translation>
-    </message>
-</context>
-<context>
-    <name>DInputDialog</name>
-    <message>
-        <location filename="../src/widgets/dinputdialog.cpp" line="42"/>
-        <source>Cancel</source>
+        <location filename="../examples/dwidget-examples/collections/cameraform.ui" line="78"/>
+        <source>mirrored</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/widgets/dinputdialog.cpp" line="43"/>
-        <source>Confirm</source>
-        <translation type="unfinished"></translation>
-    </message>
-</context>
-<context>
-    <name>DShortcutEdit</name>
-    <message>
-        <location filename="../src/widgets/dshortcutedit.cpp" line="31"/>
-        <source>Please input a new shortcut</source>
+        <location filename="../examples/dwidget-examples/collections/cameraform.ui" line="91"/>
+        <source>Round</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/widgets/dshortcutedit.cpp" line="102"/>
-        <source>None</source>
-        <translation type="unfinished"></translation>
-    </message>
-</context>
-<context>
-    <name>DSimpleListView</name>
-    <message>
-        <location filename="../src/widgets/dsimplelistview.cpp" line="1081"/>
-        <source>No search result</source>
-        <translation type="unfinished"></translation>
-    </message>
-</context>
-<context>
-    <name>QObject</name>
-    <message>
-        <location filename="../src/widgets/private/settings/content.cpp" line="181"/>
-        <source>Restore Defaults</source>
-        <translation type="unfinished"></translation>
-    </message>
-</context>
-<context>
-    <name>ShortcutEdit</name>
-    <message>
-        <location filename="../src/widgets/private/settings/shortcutedit.cpp" line="107"/>
-        <source>Please enter a new shortcut</source>
-        <translation type="unfinished"></translation>
-    </message>
-</context>
-<context>
-    <name>TitleBarMenu</name>
-    <message>
-        <location filename="../src/widgets/dtitlebar.cpp" line="225"/>
-        <source>Help</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../src/widgets/dtitlebar.cpp" line="232"/>
-        <source>About</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../src/widgets/dtitlebar.cpp" line="239"/>
-        <source>Exit</source>
+        <location filename="../examples/dwidget-examples/collections/cameraform.ui" line="104"/>
+        <source>start</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
--- translations/dtkwidget2_zh_CN.ts
+++ translations/dtkwidget2_zh_CN.ts
@@ -2,19 +2,107 @@
 <context>
     <name>DAboutDialog</name>
     <message>
-        <location filename="../src/widgets/daboutdialog.cpp" line="137"/>
+        <location filename="../src/widgets/daboutdialog.cpp" line="191"/>
         <source>Acknowledgements</source>
         <translation>鸣谢</translation>
     </message>
     <message>
-        <location filename="../src/widgets/dapplication.cpp" line="568"/>
+        <location filename="../src/widgets/dapplication.cpp" line="712"/>
         <source>Version: %1</source>
         <translation>版本：%1</translation>
     </message>
     <message>
-        <location filename="../src/widgets/dapplication.cpp" line="572"/>
+        <location filename="../src/widgets/dapplication.cpp" line="716"/>
         <source>%1 is released under %2</source>
         <translation>%1遵循%2协议发布</translation>
+    </message>
+</context>
+<context>
+    <name>DCrumbEdit</name>
+    <message>
+        <location filename="../src/widgets/dcrumbedit.cpp" line="530"/>
+        <source>Black</source>
+        <translation>黑色</translation>
+    </message>
+    <message>
+        <location filename="../src/widgets/dcrumbedit.cpp" line="533"/>
+        <source>White</source>
+        <translation>白色</translation>
+    </message>
+    <message>
+        <location filename="../src/widgets/dcrumbedit.cpp" line="536"/>
+        <source>Dark Gray</source>
+        <translation>深灰色</translation>
+    </message>
+    <message>
+        <location filename="../src/widgets/dcrumbedit.cpp" line="539"/>
+        <source>Gray</source>
+        <translation>灰色</translation>
+    </message>
+    <message>
+        <location filename="../src/widgets/dcrumbedit.cpp" line="542"/>
+        <source>Light Gray</source>
+        <translation>浅灰色</translation>
+    </message>
+    <message>
+        <location filename="../src/widgets/dcrumbedit.cpp" line="545"/>
+        <source>Red</source>
+        <translation>红色</translation>
+    </message>
+    <message>
+        <location filename="../src/widgets/dcrumbedit.cpp" line="548"/>
+        <source>Green</source>
+        <translation>绿色</translation>
+    </message>
+    <message>
+        <location filename="../src/widgets/dcrumbedit.cpp" line="551"/>
+        <source>Blue</source>
+        <translation>蓝色</translation>
+    </message>
+    <message>
+        <location filename="../src/widgets/dcrumbedit.cpp" line="554"/>
+        <source>Cyan</source>
+        <translation>青色</translation>
+    </message>
+    <message>
+        <location filename="../src/widgets/dcrumbedit.cpp" line="557"/>
+        <source>Magenta</source>
+        <translation>洋红色</translation>
+    </message>
+    <message>
+        <location filename="../src/widgets/dcrumbedit.cpp" line="560"/>
+        <source>Yellow</source>
+        <translation>黄色</translation>
+    </message>
+    <message>
+        <location filename="../src/widgets/dcrumbedit.cpp" line="563"/>
+        <source>Dark Red</source>
+        <translation>深红色</translation>
+    </message>
+    <message>
+        <location filename="../src/widgets/dcrumbedit.cpp" line="566"/>
+        <source>Dark Green</source>
+        <translation>深绿色</translation>
+    </message>
+    <message>
+        <location filename="../src/widgets/dcrumbedit.cpp" line="569"/>
+        <source>Dark Blue</source>
+        <translation>深蓝色</translation>
+    </message>
+    <message>
+        <location filename="../src/widgets/dcrumbedit.cpp" line="572"/>
+        <source>Dark Cyan</source>
+        <translation>深青色</translation>
+    </message>
+    <message>
+        <location filename="../src/widgets/dcrumbedit.cpp" line="575"/>
+        <source>Dark Magenta</source>
+        <translation>深紫红色</translation>
+    </message>
+    <message>
+        <location filename="../src/widgets/dcrumbedit.cpp" line="578"/>
+        <source>Dark Yellow</source>
+        <translation>深黄色</translation>
     </message>
 </context>
 <context>
@@ -46,7 +134,7 @@
 <context>
     <name>DSimpleListView</name>
     <message>
-        <location filename="../src/widgets/dsimplelistview.cpp" line="1082"/>
+        <location filename="../src/widgets/dsimplelistview.cpp" line="1190"/>
         <source>No search result</source>
         <translation>无搜索结果</translation>
     </message>
@@ -54,7 +142,7 @@
 <context>
     <name>QObject</name>
     <message>
-        <location filename="../src/widgets/private/settings/content.cpp" line="181"/>
+        <location filename="../src/widgets/private/settings/content.cpp" line="189"/>
         <source>Restore Defaults</source>
         <translation>恢复默认设置</translation>
     </message>
@@ -62,7 +150,7 @@
 <context>
     <name>ShortcutEdit</name>
     <message>
-        <location filename="../src/widgets/private/settings/shortcutedit.cpp" line="107"/>
+        <location filename="../src/widgets/private/settings/shortcutedit.cpp" line="119"/>
         <source>Please enter a new shortcut</source>
         <translation>请输入新的快捷键</translation>
     </message>
@@ -70,17 +158,17 @@
 <context>
     <name>TitleBarMenu</name>
     <message>
-        <location filename="../src/widgets/dtitlebar.cpp" line="342"/>
+        <location filename="../src/widgets/dtitlebar.cpp" line="449"/>
         <source>Help</source>
         <translation>帮助</translation>
     </message>
     <message>
-        <location filename="../src/widgets/dtitlebar.cpp" line="349"/>
+        <location filename="../src/widgets/dtitlebar.cpp" line="456"/>
         <source>About</source>
         <translation>关于</translation>
     </message>
     <message>
-        <location filename="../src/widgets/dtitlebar.cpp" line="356"/>
+        <location filename="../src/widgets/dtitlebar.cpp" line="463"/>
         <source>Exit</source>
         <translation>退出</translation>
     </message>
