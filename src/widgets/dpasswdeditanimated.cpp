dbc1588b15d09735738ae8ce3642fbdd44bc8ebe
listenerri <listenerri@gmail.com>
2018-09-28 14:50:13 +0800

    doc: add doc for DPasswdEditAnimated
    Change-Id: I02a6729e73c488359e78d1319f48f08e4829d8fc

--- src/widgets/dpasswdeditanimated.cpp
+++ src/widgets/dpasswdeditanimated.cpp
@@ -192,6 +192,57 @@ void DPasswdEditAnimatedPrivate::_q_resetCapslockState()
     m_caps->setVisible(false);
 }
 
+/**
+ * \~chinese \class DPasswdEditAnimated
+ * \~chinese \brief 带有动画的密码输入框。
+ *
+ * \~chinese DPasswdEditAnimated 一个带有动画，大小写标识，键盘布局按钮的密码输入框。
+ *
+ * \~chinese 动画的效果是一个半透明滑块从左向右滑动，
+ * \~chinese 可以通过 DPasswdEditAnimated::setLoadingEffectColor 来设置滑块的颜色。
+ * \~chinese 也可以通过 DPasswdEditAnimated::loadingAnimation 拿到 QPropertyAnimation 对象并设置其属性，
+ * \~chinese 以改变动画的一些行为。
+ */
+
+// ========================PROPERTY START========================
+/**
+ * \~chinese \property DPasswdEditAnimated::editFocus
+ * \~chinese \brief 获取焦点的状态
+ */
+// ========================PROPERTY END========================
+
+// ========================SIGNAL START========================
+/**
+ * \~chinese \fn DPasswdEditAnimated::submit
+ * \~chinese \brief 提交输入完成数据，一般是因为按下了回车或者点击了提交按钮。
+ * \~chinese \param input 输入的字符串。
+ */
+
+/**
+ * \~chinese \fn DPasswdEditAnimated::abort
+ * \~chinese \brief 过程被终止。
+ */
+
+/**
+ * \~chinese \fn DPasswdEditAnimated::alertChanged
+ * \~chinese \brief 属性 DPasswdEditAnimated::alert 属性发生改变。
+ */
+
+/**
+ * \~chinese \fn DPasswdEditAnimated::editFocusChanged
+ * \~chinese \brief 属性 DPasswdEditAnimated::editFocus 发生改变。
+ */
+
+/**
+ * \~chinese \fn DPasswdEditAnimated::keyboardButtonClicked
+ * \~chinese \brief 键盘布局按钮被点击。
+ */
+// ========================SIGNAL END========================
+
+/**
+ * \~chinese \brief 构造一个 DPasswdEditAnimated 实例。
+ * \~chinese \param parent 为创建对象的父控件。
+ */
 DPasswdEditAnimated::DPasswdEditAnimated(QWidget *parent) : QFrame(parent),
     DTK_CORE_NAMESPACE::DObject(*new DPasswdEditAnimatedPrivate(this))
 {
@@ -200,12 +251,25 @@ DPasswdEditAnimated::DPasswdEditAnimated(QWidget *parent) : QFrame(parent),
     d->init();
 }
 
+/**
+ * \~chinese \brief 获取输入框对象。
+ *
+ * \~chinese 拿到QLineEdit类型的输入框对象后可对其进行一些常规设置，如设置最大输入长度等。
+ * \~chinese \return 输入框对象。
+ */
 QLineEdit *DPasswdEditAnimated::lineEdit()
 {
     D_D(DPasswdEditAnimated);
 
     return d->m_passwdEdit;
 }
+
+/**
+ * \~chinese \brief 获取警告信息。
+ *
+ * \~chinese 可以用获取到对象来设置要显示的警告信息。
+ * \~chinese \return 警告信息对象。
+ */
 QLabel *DPasswdEditAnimated::invalidMessage()
 {
     D_D(DPasswdEditAnimated);
@@ -213,6 +277,12 @@ QLabel *DPasswdEditAnimated::invalidMessage()
     return d->m_invalidMessage;
 }
 
+/**
+ * \~chinese \brief 获取动画对象。
+ *
+ * \~chinese 可以用获取到的对象设置动画的属性。
+ * \~chinese \return 动画对象。
+ */
 QPropertyAnimation *DPasswdEditAnimated::loadingAnimation()
 {
     D_D(DPasswdEditAnimated);
@@ -220,6 +290,11 @@ QPropertyAnimation *DPasswdEditAnimated::loadingAnimation()
     return d->m_loadSliderAnim;
 }
 
+/**
+ * \~chinese \brief 设置是否需要键盘布局按钮。
+ *
+ * \~chinese \param value 为true表示需要，反之则反。
+ */
 void DPasswdEditAnimated::setKeyboardButtonEnable(bool value)
 {
     D_D(DPasswdEditAnimated);
@@ -227,6 +302,11 @@ void DPasswdEditAnimated::setKeyboardButtonEnable(bool value)
     d->m_keyboard->setVisible(value);
 }
 
+/**
+ * \~chinese \brief 设置是否需要大小写指示器。
+ *
+ * \~chinese \param value 为true表示需要，反之则反。
+ */
 void DPasswdEditAnimated::setCapslockIndicatorEnable(bool value)
 {
     D_D(DPasswdEditAnimated);
@@ -237,6 +317,11 @@ void DPasswdEditAnimated::setCapslockIndicatorEnable(bool value)
     }
 }
 
+/**
+ * \~chinese \brief 设置是否需要切换密码隐藏状态的按钮。
+ *
+ * \~chinese \param value 为true表示需要，反之则反。
+ */
 void DPasswdEditAnimated::setEyeButtonEnable(bool value)
 {
     D_D(DPasswdEditAnimated);
@@ -247,6 +332,11 @@ void DPasswdEditAnimated::setEyeButtonEnable(bool value)
     }
 }
 
+/**
+ * \~chinese \brief 设置是否需要提交按钮。
+ *
+ * \~chinese \param value 为true表示需要，反之则反。
+ */
 void DPasswdEditAnimated::setSubmitButtonEnable(bool value)
 {
     D_D(DPasswdEditAnimated);
@@ -257,6 +347,11 @@ void DPasswdEditAnimated::setSubmitButtonEnable(bool value)
     }
 }
 
+/**
+ * \~chinese \brief 设置是否需要动画功能。
+ *
+ * \~chinese \param value 为true表示需要，反之则反。
+ */
 void DPasswdEditAnimated::setLoadAnimEnable(bool value)
 {
     D_D(DPasswdEditAnimated);
@@ -264,6 +359,13 @@ void DPasswdEditAnimated::setLoadAnimEnable(bool value)
     d->m_loadAnimEnable = value;
 }
 
+/**
+ * \~chinese \brief 设置是密码的显示状态。
+ *
+ * \~chinese \param value 显示状态。
+ *
+ * \sa QLineEdit::EchoMode
+ */
 void DPasswdEditAnimated::setEchoMode(QLineEdit::EchoMode mode)
 {
     D_D(DPasswdEditAnimated);
@@ -271,6 +373,7 @@ void DPasswdEditAnimated::setEchoMode(QLineEdit::EchoMode mode)
     d->m_passwdEdit->setEchoMode(mode);
 }
 
+/** \reimp */
 bool DPasswdEditAnimated::eventFilter(QObject *watched, QEvent *event)
 {
     D_D(DPasswdEditAnimated);
@@ -290,6 +393,12 @@ bool DPasswdEditAnimated::eventFilter(QObject *watched, QEvent *event)
     return false;
 }
 
+/**
+ * \~chinese \brief 显示警告信息。
+ *
+ * \~chinese 弹出一个警告信息提示框。
+ * \~chinese \param message 要显示的信息。
+ */
 void DPasswdEditAnimated::showAlert(const QString &message)
 {
     D_D(DPasswdEditAnimated);
@@ -319,6 +428,9 @@ void DPasswdEditAnimated::showAlert(const QString &message)
     }
 }
 
+/**
+ * \~chinese \brief 隐藏警告提示框。
+ */
 void DPasswdEditAnimated::hideAlert()
 {
     D_D(DPasswdEditAnimated);
@@ -332,6 +444,9 @@ void DPasswdEditAnimated::hideAlert()
 
 }
 
+/**
+ * \~chinese \brief 终止认证过程，同时会关闭警告提示框和停止动画。
+ */
 void DPasswdEditAnimated::abortAuth()
 {
     D_D(DPasswdEditAnimated);
@@ -342,6 +457,11 @@ void DPasswdEditAnimated::abortAuth()
     }
 }
 
+/**
+ * \~chinese \property DPasswdEditAnimated::alert
+ * \~chinese \brief 警告信息的显示状态。
+ * \~chinese \return true 正在显示警告信息，反之则反。
+ */
 bool DPasswdEditAnimated::alert()
 {
     D_D(DPasswdEditAnimated);
@@ -349,6 +469,14 @@ bool DPasswdEditAnimated::alert()
     return d->m_invalidTip->isVisible();
 }
 
+/**
+ * \~chinese \brief 设置提交按钮的图标。
+ *
+ * \~chinese 参数可以是文件系统中的文件路径，也可以是资源系统中的路径。
+ * \~chinese \param normalPic 正常状态下的图标。
+ * \~chinese \param hoverPic 鼠标悬浮状态下的图标。
+ * \~chinese \param pressPic 鼠标按下状态下的图标。
+ */
 void DPasswdEditAnimated::setSubmitIcon(const QString &normalPic, const QString &hoverPic, const QString &pressPic)
 {
     D_D(DPasswdEditAnimated);
@@ -358,6 +486,11 @@ void DPasswdEditAnimated::setSubmitIcon(const QString &normalPic, const QString 
     d->m_submit->setPressPic(pressPic);
 }
 
+/**
+ * \~chinese \property DPasswdEditAnimated::loadingEffectColor
+ * \~chinese \brief 动画的颜色。
+ * \~chinese \sa DPasswdEditAnimated::setLoadingEffectColor
+ */
 QColor DPasswdEditAnimated::loadingEffectColor()
 {
     D_D(DPasswdEditAnimated);
@@ -365,6 +498,11 @@ QColor DPasswdEditAnimated::loadingEffectColor()
     return d->m_loadSlider->loadSliderColor();
 }
 
+/**
+ * \~chinese \brief 设置动画的颜色。
+ *
+ * \~chinese \param color 动画的颜色。
+ */
 void DPasswdEditAnimated::setLoadingEffectColor(const QColor &color)
 {
     D_D(DPasswdEditAnimated);
