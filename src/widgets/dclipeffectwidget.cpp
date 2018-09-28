3c5c8647436f73d52fe6fd63ab2501fe01ca7476
zccrs <ccrr1314@live.com>
2018-09-28 19:16:47 +0800

    docs: add document for DClipEffectWidget
    Change-Id: I27dfbdbbc3cc4676c3d13d583a28181bcecdd4d0

--- src/widgets/dclipeffectwidget.cpp
+++ src/widgets/dclipeffectwidget.cpp
@@ -52,15 +52,60 @@ DClipEffectWidgetPrivate::DClipEffectWidgetPrivate(DClipEffectWidget *qq)
 }
 
 /*!
- * \class DClipEffectWidget
- * \brief The DClipEffectWidget class clips itself and its content according to a given clip path.
+ * \~chinese \class DClipEffectWidget
+ * \~chinese \brief 用于裁剪窗口的绘制内容
  *
- * This class is useful when you want to make rounded widget or things like that.
+ * \~chinese 支持使用 QPainterPath 设置一个区域，位于区域外的窗口内容将被裁剪。被裁剪的对象
+ * \~chinese 是此控件的父控件, 且 DClipEffectWidget 会自动填充整个父控件，另外可以通过设置
+ * \~chinese \ref margins 控制有效的裁剪区域。如下图所示：
+ * \htmlonly
+ * <pre style="font-family: FreeMono, Consolas, Menlo, 'Noto Mono', 'Courier New', Courier, monospace;line-height: 100%;">
+ * ┏━━━━━━━━━━━━━━┯━━━━━━━━━━━━━━┓
+ * ┃              │              ┃
+ * ┃              │              ┃
+ * ┃     A        m              ┃
+ * ┃              │              ┃
+ * ┃              │              ┃
+ * ┃       ┏┅┅┅┅┅┅┷┅┅┅┅┅┅┓       ┃
+ * ┃       ┋B ╭───────╮  ┋       ┃
+ * ┃       ┋  │       │  ┋       ┃
+ * ┃── m ──┋  │   C   │  ┋── m ──┃
+ * ┃       ┋  │       │  ┋       ┃
+ * ┃       ┋  ╰───────╯  ┋       ┃
+ * ┃       ┗┅┅┅┅┅┅┯┅┅┅┅┅┅┛       ┃
+ * ┃              │              ┃
+ * ┃              │              ┃
+ * ┃              m              ┃
+ * ┃              │              ┃
+ * ┃              │              ┃
+ * ┗━━━━━━━━━━━━━━┷━━━━━━━━━━━━━━┛
+ * </pre>
+ * \endhtmlonly
+ * \~chinese A为父控件区域， DClipEffectWidget 控件区域和父控件相同，B为有效的被裁剪区域，C
+ * \~chinese 为 \ref clipPath 区域，m为 \ref margins ，则被裁剪掉的区域为： B - C，裁掉的部分将显示
+ * \~chinese 下层控件内容，如果下层没有其它控件，将显示主窗口背景。
+ *
+ * \~chinese 此控件不接收任何输入事件，且不接受焦点
+ * \sa Qt::WA_TransparentForMouseEvents Qt::WidgetAttribute::NoFocus
+ * \sa DGraphicsClipEffect
  */
 
 /*!
- * \brief DClipEffectWidget::DClipEffectWidget constructs an instance of DClipEffectWidget.
- * \param parent is passed to QWidget constructor.
+  * \fn void DClipEffectWidget::marginsChanged(QMargins margins)
+  * \~chinese 这个信号在 \ref margins 改变时被发送
+  */
+
+/*!
+  * \fn void DClipEffectWidget::clipPathChanged(QPainterPath clipPath)
+  * \~chinese 这个信号在 \ref clipPath 改变时被发送
+  */
+
+
+/*!
+ * \brief 构造 DClipEffectWidget 对象，和普通 QWidget 一样，可以传入一个 QWidget 对象
+ * \~chinese 指针作为其父对象
+ * \~chinese \param 父对象
+* \~chinese \warning 必须要传入一个有效的父对象，将此控件作为顶级窗口没有任何意义
  */
 DClipEffectWidget::DClipEffectWidget(QWidget *parent)
     : QWidget(parent)
@@ -74,7 +119,7 @@ DClipEffectWidget::DClipEffectWidget(QWidget *parent)
 
 /*!
  * \property DClipEffectWidget::margins
- * \brief This property holds the margin need to be removed before the clip.
+ * \~chinese \brief 用于调整控件的被裁剪区域，可以和 \ref clipPath 组合更加灵活的裁剪控件
  */
 QMargins DClipEffectWidget::margins() const
 {
@@ -85,7 +130,9 @@ QMargins DClipEffectWidget::margins() const
 
 /*!
  * \property DClipEffectWidget::clipPath
- * \brief This property holds the path used to clip the widget.
+ * \~chinese \brief 被裁剪的区域，控件真实裁剪区域 = \ref clipPath & (\ref rect() - \ref margins)
+ * \~chinese \warning clipPath 必须是一个封闭的环状路径
+ * \sa QPainterPath::closeSubpath
  */
 QPainterPath DClipEffectWidget::clipPath() const
 {
@@ -94,6 +141,9 @@ QPainterPath DClipEffectWidget::clipPath() const
     return d->path;
 }
 
+/*!
+ * \~chinese \param margins
+ */
 void DClipEffectWidget::setMargins(QMargins margins)
 {
     D_D(DClipEffectWidget);
@@ -107,6 +157,9 @@ void DClipEffectWidget::setMargins(QMargins margins)
     Q_EMIT marginsChanged(margins);
 }
 
+/*!
+ * \~chinese \param path
+ */
 void DClipEffectWidget::setClipPath(const QPainterPath &path)
 {
     D_D(DClipEffectWidget);
