63df04970fb41f370d6b8672b2c519eb9dbc4cbd
Hualet Wang <mr.asianwang@gmail.com>
2018-10-11 15:42:32 +0800

    docs: document DPageIndicator
    Change-Id: Ib99a0a14c04cf2dedca52aebd44cc3c191b426ce

--- src/widgets/dpageindicator.cpp
+++ src/widgets/dpageindicator.cpp
@@ -24,22 +24,39 @@
 DWIDGET_BEGIN_NAMESPACE
 
 /*!
- * \class DPageIndicator
- * \brief The DPageIndicator class provides indicator of which page is currently showing.
+ * \~english \class DPageIndicator
+ * \~english \brief The DPageIndicator class provides indicator of which page is currently showing.
  *
- * DPageIndicator is control that displays a horizontal series of dots,
- * each of which corresponds to a page/widget may appear on top of it.
+ * \~english DPageIndicator is control that displays a horizontal series of dots,
+ * \~english each of which corresponds to a page/widget may appear on top of it.
  *
- * It's inspired by UIPageControl of Apple.
- * See https://developer.apple.com/documentation/uikit/uipagecontrol.
+ * \~english It's inspired by UIPageControl of Apple.
+ * \~english See https://developer.apple.com/documentation/uikit/uipagecontrol.
  *
- * \note The design of this control is that it's just used to display, not
- * to control which page is currently visible.
+ * \~english \note The design of this control is that it's just used to display, not
+ * \~english to control which page is currently visible.
+ *
+ *
+ * \~chinese \class DPageIndicator
+ * \~chinese \brief DPageIndicator 类提供了一种用来指示一组元素中当前可见元素索引的控件。
+ * \~chinese
+ * \~chinese 控件会显示了一系列的点，代表一组元素。其中每一个点固定对应于元素组中
+ * \~chinese 的一个元素，元素组中的元素在一个时间点只能显示其中的一个（ 如 \ref QStackedLayout ），
+ * \~chinese 当前显示的元素所对应的点会被高亮以告知用户元素左右（或者上下）还有多少元素可以切换。
+ * \~chinese
+ * \~chinese 例如下图中控制中心首页插件区域底部的指示器：
+ * \~chinese \image html pageindicator.png
+ * \~chinese
+ * \~chinese \note 此控件只用来显示当前元素位置以及层叠关系，并不能用来实际操纵元素的显示行为。
  */
 
 /*!
- * \brief DPageIndicator::DPageIndicator constructs an instance of DPageIndicator.
- * \param parent is passed to QWidget constructor.
+ * \~english \brief DPageIndicator::DPageIndicator constructs an instance of DPageIndicator.
+ * \~english \param parent is passed to QWidget constructor.
+ *
+ *
+ * \~chinese \brief DPageIndicator::DPageIndicator 是 DPageIndicator 类的构造函数。
+ * \~chinese \param parent 指定了控件的父控件。
  */
 DPageIndicator::DPageIndicator(QWidget *parent)
     : QWidget(parent),
@@ -48,8 +65,12 @@ DPageIndicator::DPageIndicator(QWidget *parent)
 }
 
 /*!
- * \property DPageIndicator::pageCount
- * \brief This property holds the number of pages that are represented by this widget.
+ * \~english \property DPageIndicator::pageCount
+ * \~english \brief This property holds the number of pages that are represented by this widget.
+ *
+ *
+ * \~chinese \property DPageIndicator::pageCount
+ * \~chinese \brief DPageIndicator::pageCount 属性代表了控件显示的点的数量。
  */
 int DPageIndicator::pageCount() const
 {
@@ -66,9 +87,12 @@ void DPageIndicator::setPageCount(const int count)
 }
 
 /*!
- * \brief DPageIndicator::nextPage highlights the next dot.
+ * \~english \brief DPageIndicator::nextPage highlights the next dot.
  *
- * \see DPageIndicator::setCurrentPage.
+ *
+ * \~chinese \brief DPageIndicator::nextPage 将高亮位置移至下一个点。
+ *
+ * \sa currentPage.
  */
 void DPageIndicator::nextPage()
 {
@@ -78,9 +102,12 @@ void DPageIndicator::nextPage()
 }
 
 /*!
- * \brief DPageIndicator::previousPage hightlights the previous dot.
+ * \~english \brief DPageIndicator::previousPage hightlights the previous dot.
  *
- * \see DPageIndicator::setCurrentPage.
+ *
+ * \~chinese \brief DPageIndicator::previousPage 将高亮位置移至前一个点。
+ *
+ * \see currentPage.
  */
 void DPageIndicator::previousPage()
 {
@@ -97,8 +124,12 @@ void DPageIndicator::setCurrentPage(const int index)
 }
 
 /*!
- * \property DPageIndicator::currentPage
- * \brief This property holds the index of currently highlighted dot.
+ * \~english \property DPageIndicator::currentPage
+ * \~english \brief This property holds the index of currently highlighted dot.
+ *
+ *
+ * \~chinese \property DPageIndicator::currentPage
+ * \~chinese \brief DPageIndicator::currentPage 属性代表了当前高亮的点在所有点中的索引。
  */
 int DPageIndicator::currentPageIndex() const
 {
@@ -108,8 +139,12 @@ int DPageIndicator::currentPageIndex() const
 }
 
 /*!
- * \property DPageIndicator::pointColor
- * \brief This property holds the color used to draw the highlighted dot.
+ * \~english \property DPageIndicator::pointColor
+ * \~english \brief This property holds the color used to draw the highlighted dot.
+ *
+ *
+ * \~chinese \property DPageIndicator::pointColor
+ * \~chinese \brief DPageIndicator::pointColor 属性用来控制高亮点的颜色。
  */
 QColor DPageIndicator::pointColor() const
 {
@@ -126,8 +161,12 @@ void DPageIndicator::setPointColor(QColor color)
 }
 
 /*!
- * \property DPageIndicator::secondaryPointColor
- * \brief This property holds the color used to draw the noraml dots.
+ * \~english \property DPageIndicator::secondaryPointColor
+ * \~english \brief This property holds the color used to draw the noraml dots.
+ *
+ *
+ * \~chinese \property DPageIndicator::secondaryPointColor
+ * \~chinese \brief DPageIndicator::secondaryPointColor 属性用来控制默认点的颜色。
  */
 QColor DPageIndicator::secondaryPointColor() const
 {
@@ -144,8 +183,12 @@ void DPageIndicator::setSecondaryPointColor(QColor color)
 }
 
 /*!
- * \property DPageIndicator::pointRadius
- * \brief This property holds the radius of the highlighted dot.
+ * \~english \property DPageIndicator::pointRadius
+ * \~english \brief This property holds the radius of the highlighted dot.
+ *
+ *
+ * \~chinese \property DPageIndicator::pointRadius
+ * \~chinese \brief DPageIndicator::pointRadius 属性用来控制高亮点的大小（半径）。
  */
 int DPageIndicator::pointRadius() const
 {
@@ -160,8 +203,12 @@ void DPageIndicator::setPointRadius(int size)
 }
 
 /*!
- * \property DPageIndicator::secondaryPointRadius
- * \brief This property holds the radius of the normal dot.
+ * \~english \property DPageIndicator::secondaryPointRadius
+ * \~english \brief This property holds the radius of the normal dot.
+ *
+ *
+ * \~chinese \property DPageIndicator::secondaryPointRadius
+ * \~chinese \brief DPageIndicator::secondaryPointRadius 属性用来控制普通点的大小（半径）。
  */
 int DPageIndicator::secondaryPointRadius() const
 {
@@ -176,8 +223,12 @@ void DPageIndicator::setSecondaryPointRadius(int size)
 }
 
 /*!
- * \property DPageIndicator::pointDistance
- * \brief This property holds the distance between two dots.
+ * \~english \property DPageIndicator::pointDistance
+ * \~english \brief This property holds the distance between two dots.
+ *
+ *
+ * \~chinese \property DPageIndicator::pointDistance
+ * \~chinese \brief DPageIndicator::pointDistance 属性用来控制两个点之间的距离。
  */
 int DPageIndicator::pointDistance() const
 {
