7edd27d428c23d54719a4651dde1bdceed296a36
Gary Wang <wzc782970009@gmail.com>
2018-10-11 15:25:15 +0800

    doc: DBaseLine and DHeaderLine
    Change-Id: I693b66366a8d82836dc73ae46e0de9ab1d3ffd10

--- src/widgets/dbaseline.cpp
+++ src/widgets/dbaseline.cpp
@@ -20,6 +20,24 @@
 
 DWIDGET_BEGIN_NAMESPACE
 
+/*!
+    \~chinese \class DBaseLine
+    \~chinese \brief 可以使用 DBaseLine 类快速创建具有位于左右两侧的两个控件的行控件。
+
+    \~chinese DBaseLine 提供一个固定高度的控件，包含分别位于左侧和右侧的两个可供显示的控件。
+
+    \~chinese \sa DHeaderLine
+*/
+
+/*!
+    \~english \class DBaseLine
+    \~english \brief Use DBaseLine to create a widget for display inline widget.
+
+    \~english DBaseLine provide a fixed height inline widget with two widgets for displaying related stuff.
+
+    \~english \sa DHeaderLine
+*/
+
 DBaseLine::DBaseLine(QWidget *parent) : QLabel(parent)
 {
     DThemeManager::registerWidget(this);
@@ -41,6 +59,10 @@ DBaseLine::DBaseLine(QWidget *parent) : QLabel(parent)
     this->setLayout(m_mainLayout);
 }
 
+/*!
+ * \~english \brief set left content widget
+ * \~chinese \brief 设置左侧内容控件
+ */
 void DBaseLine::setLeftContent(QWidget *content)
 {
     QLayoutItem *child;
@@ -51,6 +73,10 @@ void DBaseLine::setLeftContent(QWidget *content)
     m_leftLayout->addWidget(content);
 }
 
+/*!
+ * \~english \brief set right content widget
+ * \~chinese \brief 设置右侧内容控件
+ */
 void DBaseLine::setRightContent(QWidget *content)
 {
     QLayoutItem *child;
@@ -61,33 +87,57 @@ void DBaseLine::setRightContent(QWidget *content)
     m_rightLayout->addWidget(content);
 }
 
+/*!
+ * \~english \brief get left layout
+ * \~chinese \brief 获取左侧 QBoxLayout
+ */
 QBoxLayout *DBaseLine::leftLayout()
 {
     return m_leftLayout;
 }
 
+/*!
+ * \~english \brief get right layout
+ * \~chinese \brief 获取右侧 QBoxLayout
+ */
 QBoxLayout *DBaseLine::rightLayout()
 {
     return m_rightLayout;
 }
 
+/*!
+ * \~english \brief set left margin
+ * \~chinese \brief 设置左侧外补(margin)边距
+ */
 void DBaseLine::setLeftMargin(int margin)
 {
     m_leftMargin = margin;
     m_mainLayout->setContentsMargins(m_leftMargin, 0, m_rightMargin, 0);
 }
 
+/*!
+ * \~english \brief set right margin
+ * \~chinese \brief 设置右侧外补(margin)边距
+ */
 void DBaseLine::setRightMargin(int margin)
 {
     m_rightMargin = margin;
     m_mainLayout->setContentsMargins(m_leftMargin, 0, m_rightMargin, 0);
 }
 
+/*!
+ * \~english \brief get left margin
+ * \~chinese \brief 左侧外补(margin)边距
+ */
 int DBaseLine::leftMargin() const
 {
     return m_leftMargin;
 }
 
+/*!
+ * \~english \brief get right margin
+ * \~chinese \brief 右侧外补(margin)边距
+ */
 int DBaseLine::rightMargin() const
 {
     return m_rightMargin;
--- src/widgets/dheaderline.cpp
+++ src/widgets/dheaderline.cpp
@@ -20,6 +20,25 @@
 
 DWIDGET_BEGIN_NAMESPACE
 
+/*!
+    \~chinese \class DHeaderLine
+    \~chinese \brief 可以使用 DHeaderLine 类快速创建标题行控件。
+
+    \~chinese DHeaderLine 提供一个固定高度的控件可供显示标题或相关内容，其行内提供左对齐的文字和可选的位于右侧的控件。
+
+    \~chinese \sa DBaseLine
+*/
+
+/*!
+    \~english \class DHeaderLine
+    \~english \brief Use DHeaderLine to create a widget for display header title.
+
+    \~english DHeaderLine provide a fixed height header line to display header title or related stuff.
+    Title are aligned left and you can also set an optional right aligned QWidget.
+
+    \~english \sa DBaseLine
+*/
+
 DHeaderLine::DHeaderLine(QWidget *parent) : DBaseLine(parent)
 {
     DThemeManager::registerWidget(this);
@@ -31,17 +50,36 @@ DHeaderLine::DHeaderLine(QWidget *parent) : DBaseLine(parent)
     DBaseLine::setLeftContent(m_titleLabel);
 }
 
+/*!
+ * \~english \brief Set the title of the header line.
+ * \~chinese \brief 设置标题。
+ *
+ * \sa title()
+ */
 void DHeaderLine::setTitle(const QString &title)
 {
     m_titleLabel->setText(title);
 }
 
+/*!
+ * \~english \brief Set the content widget of the header line.
+ * \~chinese \brief 设置内容控件。
+ *
+ * \~english Content widget aligned right and the height is limited because of the fixed height.
+ * \~chinese 内容控件将右对齐，且高度受限于 DHeaderLine 本身的固定高度。
+ */
 void DHeaderLine::setContent(QWidget *content)
 {
     DBaseLine::setRightContent(content);
     setFixedHeight(CONTENT_HEADER_HEIGHT);
 }
 
+/*!
+ * \~english \brief Title of the header line.
+ * \~chinese \brief 获取标题。
+ *
+ * \sa title()
+ */
 QString DHeaderLine::title() const
 {
     return m_titleLabel->text();
