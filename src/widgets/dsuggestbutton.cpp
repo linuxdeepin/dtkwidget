b172c013843304217188a541e8b38285a09b9a28
haruyukilxz <lxz@ilxz.me>
2018-10-13 14:21:54 +0800

    doc: add chinese documentation for DSuggestButton
    Change-Id: I63723a224bf075644df87dab37e767536700316d

--- src/widgets/dsuggestbutton.cpp
+++ src/widgets/dsuggestbutton.cpp
@@ -6,12 +6,28 @@
 
 DWIDGET_BEGIN_NAMESPACE
 
+/*!
+ * \~chinese \class DSuggestButton
+ * \~chinese \brief 这个控件提供了DTK中默认的按钮样式.
+ */
+
+/*!
+ * \~chinese \brief DSuggestButton的构造函数
+ *
+ * @param parent
+ */
 DSuggestButton::DSuggestButton(QWidget *parent)
     : QPushButton(parent)
 {
 
 }
 
+/*!
+ * \~chinese \brief DSuggestButton的重载构造函数，接收默认显示的文字
+ *
+ * @param text 默认显示的文字
+ * @param parent
+ */
 DSuggestButton::DSuggestButton(const QString &text, QWidget *parent)
     : QPushButton(text, parent)
 {
