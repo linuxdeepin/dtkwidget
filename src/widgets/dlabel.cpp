59cd9327d581a95cedacf1755974e09cc883a30f
rekols <rekols@foxmail.com>
2018-10-14 15:55:39 +0800

    doc: add chinese documentation for DLabel.
    Change-Id: I57eb7221e4fdf403a9ffbfe32b572709978026c2

--- src/widgets/dlabel.cpp
+++ src/widgets/dlabel.cpp
@@ -21,15 +21,26 @@
 DWIDGET_BEGIN_NAMESPACE
 
 /*!
- * \class DLabel
- * \brief The DLabel class is a direct subclass of QLabel, styled by deepin to
+ * \~english \class DLabel
+ * \~english \brief The DLabel class is a direct subclass of QLabel, styled by deepin to
  * provide better style unification.
  */
 
 /*!
- * \brief DLabel::DLabel constructs an instance of DLabel.
- * \param parent is passed to QLabel constructor.
- * \param f is passed to QLabel constructor.
+ * \~chinese \class DLabel
+ * \~chinese \brief DLabel 是 QLabel 的子类，为 deepin 提供更好的的风格统一。
+ */
+
+/*!
+ * \~english \brief DLabel::DLabel constructs an instance of DLabel.
+ * \~english \param parent is passed to QLabel constructor.
+ * \~english \param f is passed to QLabel constructor.
+ */
+
+/*!
+ * \~chinese \brief DLabel 的构造函数.
+ * \~chinese \param parent
+ * \~chinese \param f
  */
 DLabel::DLabel(QWidget *parent, Qt::WindowFlags f)
     : QLabel(parent, f)
@@ -38,10 +49,17 @@ DLabel::DLabel(QWidget *parent, Qt::WindowFlags f)
 }
 
 /*!
- * \brief DLabel::DLabel constructs an instance of DLabel.
- * \param text is used to initialize the content, passed to QLabel constructor.
- * \param parent is passed to QLabel constructor.
- * \param f is passed to QLabel constructor.
+ * \~english \brief DLabel::DLabel constructs an instance of DLabel.
+ * \~english \param text is used to initialize the content, passed to QLabel constructor.
+ * \~english \param parent is passed to QLabel constructor.
+ * \~english \param f is passed to QLabel constructor.
+ */
+
+/*!
+ * \~chinese \brief DLabel 的重载构造函数
+ * \~chinese \param text 用于初始化本文内容，传递给 QLabel 构造函数
+ * \~chinese \param parent 初始化 QLabel 的构造函数
+ * \~chinese \param f 初始化 QLabel 的构造函数
  */
 DLabel::DLabel(const QString &text, QWidget *parent, Qt::WindowFlags f)
     : QLabel(text, parent, f)
