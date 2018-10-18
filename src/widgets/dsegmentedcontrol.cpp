dc08b64a4fbc6668c35fffc3e78816707751a228
Hualet Wang <mr.asianwang@gmail.com>
2018-10-18 14:23:16 +0800

    docs: document DSegmentedControl
    Change-Id: If3d4fa362f55a4567f88a49dfae0114ffebf2430

--- src/widgets/dsegmentedcontrol.cpp
+++ src/widgets/dsegmentedcontrol.cpp
@@ -24,11 +24,54 @@
 
 DWIDGET_BEGIN_NAMESPACE
 
+/*!
+ * \~chinese \class DSegmentedHighlight
+ * \~chinese \brief DSegmentedHighlight 类代表了 DSegmentedControl 中当前选中（即高亮）的部分。
+ */
+
+/*!
+ * \~chinese \brief DSegmentedHighlight::DSegmentedHighlight 为 DSegmentedHighlight 类的构造函数。
+ * \~chinese \param parent 制定了控件的父控件。
+ *
+ * \~chinese \note 一般使用者无需使用自己构造 DSegmentedHighlight 对象。
+ */
 DSegmentedHighlight::DSegmentedHighlight(QWidget *parent) :
     QToolButton(parent)
 {
 }
 
+/*!
+ * \~chinese \class DSegmentedControl
+ * \~chinese \brief DSegmentedControl 类提供了一种类似标签页的控件。
+ * \~chinese 它由一系列的 Segment 构成，每一个 Segment 可以被设置标题和图标，也可以被选中，
+ * \~chinese 它们之间的选中为互斥关系。
+ * \~chinese DSegmentedControl 更像是脱离于 QTabWidget 工作的 QTabBar 。
+ * \~chinese
+ * \~chinese 它可以跟 QStackedLayout 搭配使用，达到切换标签页的功能，例如：
+ * \~chinese \image html segmentedcontrol.png
+ */
+
+
+/*!
+ * \~chinese \fn DSegmentedControl::currentChanged(int index)
+ * \~chinese \brief 信号在当前选中（高亮）的 Segment 发生改变时触发。
+ */
+
+/*!
+ * \~chinese \fn DSegmentedControl::currentTitleChanged(QString title)
+ * \~chinese \brief 信号在当前选中（高亮）的 Segment 发生改变时触发。
+ */
+
+/*!
+ * \~chinese \fn DSegmentedControl::animationDurationChanged(int animationDuration)
+ * \~chinese \brief 信号在高亮动画的动画周期发生改变时触发。
+ */
+
+
+/*!
+ * \~chinese \brief DSegmentedControl::DSegmentedControl 为 DSegmentedControl 类的构造函数。
+ * \~chinese \param parent 制定了控件的父控件。
+ */
 DSegmentedControl::DSegmentedControl(QWidget *parent) :
     QFrame(parent),
     m_highlight(new DSegmentedHighlight(this)),
@@ -51,26 +94,49 @@ DSegmentedControl::DSegmentedControl(QWidget *parent) :
     m_highlightMoveAnimation->setEasingCurve(QEasingCurve::InCubic);
 }
 
+/*!
+ * \~chinese \property DSegmentedControl::count
+ * \~chinese \brief count 属性代表了控件中 Segment 的数量。
+ */
 int DSegmentedControl::count() const
 {
     return m_tabList.count();
 }
 
+/*!
+ * \~chinese \brief DSegmentedControl::highlight 用于获取当前选中（高亮）的 Segment 。
+ * \~chinese \return 控件中当前选中（高亮）的 Segmnet 。
+ */
 const DSegmentedHighlight *DSegmentedControl::highlight() const
 {
     return m_highlight;
 }
 
+/*!
+ * \~chinese \property DSegmentedControl::currentIndex
+ * \~chinese \brief currentIndex 属性指示当前选中 Segment 在所有 Segment 中的位置。
+ */
 int DSegmentedControl::currentIndex() const
 {
     return m_currentIndex;
 }
 
+/*!
+ * \~chinese \brief DSegmentedControl::at 用于获取在特定位置上的 Segment。
+ * \~chinese Segment 目前由 QToolButton 充当。
+ * \~chinese \param index 为所要获取的 Segment 的索引。
+ * \~chinese \return 指定索引位置的 Segment。
+ */
 QToolButton *DSegmentedControl::at(int index) const
 {
     return m_tabList[index];
 }
 
+/*!
+ * \~chinese \brief DSegmentedControl::getText 用于获取在特定位置上 Segment 的标题。
+ * \~chinese \param index 为所要获取的 Segment 的索引。
+ * \~chinese \return 指定索引位置上 Segment 的标题。
+ */
 QString DSegmentedControl::getText(int index) const
 {
     const QToolButton *button = at(index);
@@ -82,16 +148,31 @@ QString DSegmentedControl::getText(int index) const
     return "";
 }
 
+/*!
+ * \~chinese \brief DSegmentedControl::getIcon 用于获取在特定位置上 Segment 的图标。
+ * \~chinese \param index 为所要获取的 Segment 的索引。
+ * \~chinese \return 指定索引位置上 Segment 的图标。
+ */
 QIcon DSegmentedControl::getIcon(int index) const
 {
     return at(index)->icon();
 }
 
+/*!
+ * \~chinese \property DSegmentedControl::animationDuration
+ * \~chinese \brief animationDuration 属性用于控制控件中高亮部分移动动画的时间周期。
+ */
 int DSegmentedControl::animationDuration() const
 {
     return m_highlightMoveAnimation->duration();
 }
 
+/*!
+ * \~chinese \brief DSegmentedControl::indexByTitle 用于通过 Segment 的标题内容获取
+ * \~chinese Segment 的索引。
+ * \~chinese \param title 指定 Segment 的标题内容。
+ * \~chinese \return 指定 Segment 的索引。
+ */
 int DSegmentedControl::indexByTitle(const QString &title) const
 {
     int i=0;
@@ -104,11 +185,23 @@ int DSegmentedControl::indexByTitle(const QString &title) const
     return -1;
 }
 
+/*!
+ * \~chinese \property DSegmentedControl::animationType
+ * \~chinese \brief animationType 属性用于控制控件中高亮部分移动动画的动画曲线类型。
+ */
 QEasingCurve::Type DSegmentedControl::animationType() const
 {
     return m_highlightMoveAnimation->easingCurve().type();
 }
 
+/*!
+ * \~chinese \brief DSegmentedControl::addSegmented 为控件添加 Segment，并指定标题。
+ * \~chinese \param title 为 Segment 的标题。
+ * \~chinese \return 新增加的 Segment 的索引。
+ *
+ *
+ * \sa insertSegmented, removeSegmented
+ */
 int DSegmentedControl::addSegmented(const QString &title)
 {
     insertSegmented(m_hLayout->count(), title);
@@ -116,6 +209,14 @@ int DSegmentedControl::addSegmented(const QString &title)
     return m_hLayout->count()-1;
 }
 
+/*!
+ * \~chinese \brief DSegmentedControl::addSegmented 为控件添加 Segment，并指定标题和图标。
+ * \~chinese \param icon 为 Segment 的图标。
+ * \~chinese \param title 为 Segment 的标题。
+ * \~chinese \return 新增加的 Segment 的索引。
+ *
+ * \sa insertSegmented, removeSegmented
+ */
 int DSegmentedControl::addSegmented(const QIcon &icon, const QString &title)
 {
     insertSegmented(m_hLayout->count(), icon, title);
@@ -123,6 +224,13 @@ int DSegmentedControl::addSegmented(const QIcon &icon, const QString &title)
     return m_hLayout->count()-1;
 }
 
+/*!
+ * \~chinese \brief DSegmentedControl::addSegmented 添加多个 Segment。
+ * \~chinese \param titleList 多个 Segment 的标题列表。
+ *
+ *
+ * \sa insertSegmented, removeSegmented
+ */
 void DSegmentedControl::addSegmented(const QStringList &titleList)
 {
     Q_FOREACH (const QString &title, titleList) {
@@ -130,6 +238,14 @@ void DSegmentedControl::addSegmented(const QStringList &titleList)
     }
 }
 
+/*!
+ * \~chinese \brief DSegmentedControl::addSegmented 添加多个 Segment。
+ * \~chinese \param titleList 为多个 Segment 的标题列表。
+ * \~chinese \param iconList 为多个 Segment 的图标列表。
+ *
+ *
+ * \sa insertSegmented, removeSegmented
+ */
 void DSegmentedControl::addSegmented(const QList<QIcon> &iconList, const QStringList &titleList)
 {
     for(int i=0;i<titleList.count();++i){
@@ -137,11 +253,27 @@ void DSegmentedControl::addSegmented(const QList<QIcon> &iconList, const QString
     }
 }
 
+/*!
+ * \~chinese \brief DSegmentedControl::insertSegmented 用于在特定位置插入一个新的 Segment。
+ * \~chinese \param index 指定要插入 Segment 的位置。
+ * \~chinese \param title 指定要插入 Segment 的标题。
+ *
+ *
+ * \sa addSegmented, removeSegmented
+ */
 void DSegmentedControl::insertSegmented(int index, const QString &title)
 {
     insertSegmented(index, QIcon(), title);
 }
 
+/*!
+ * \~chinese \brief DSegmentedControl::insertSegmented 用于在特定位置插入一个新的 Segment。
+ * \~chinese \param index 指定要插入 Segment 的位置。
+ * \~chinese \param title 指定要插入 Segment 的标题。
+ *
+ *
+ * \sa addSegmented, removeSegmented
+ */
 void DSegmentedControl::insertSegmented(int index, const QIcon &icon, const QString &title)
 {
     QToolButton *button = new QToolButton();
@@ -163,6 +295,10 @@ void DSegmentedControl::insertSegmented(int index, const QIcon &icon, const QStr
     button->installEventFilter(this);
 }
 
+/*!
+ * \~chinese \brief DSegmentedControl::removeSegmented 用于移除特定位置上的 Segment。
+ * \~chinese \param index 指定了要移除的 Segment 的索引。
+ */
 void DSegmentedControl::removeSegmented(int index)
 {
     if(index == m_currentIndex)
@@ -176,6 +312,9 @@ void DSegmentedControl::removeSegmented(int index)
         button->deleteLater();
 }
 
+/*!
+ * \~chinese \brief DSegmentedControl::clear 清除所有 Segment 。
+ */
 void DSegmentedControl::clear()
 {
     for(int i=0; i<count(); ++i){
@@ -216,16 +355,31 @@ bool DSegmentedControl::setCurrentIndex(int currentIndex)
     return true;
 }
 
+/*!
+ * \~chinese \brief DSegmentedControl::setCurrentIndexByTitle 设置标题为指定标题的 Segment 为当前选中（高亮）的 Segment。
+ * \~chinese \param title 为指定的标题。
+ * \~chinese \return 是否设置成功， true 为设置成功，否则为 false 。
+ */
 bool DSegmentedControl::setCurrentIndexByTitle(const QString &title)
 {
     return setCurrentIndex(indexByTitle(title));
 }
 
+/*!
+ * \~chinese \brief DSegmentedControl::setText 用于给特定位置的 Segment 设置标题。
+ * \~chinese \param index 为指定 Segment 的索引。
+ * \~chinese \param title 为要设定的标题内容。
+ */
 void DSegmentedControl::setText(int index, const QString &title)
 {
     at(index)->setText(title);
 }
 
+/*!
+ * \~chinese \brief DSegmentedControl::setIcon 用于给特定位置的 Segment 设置图标。
+ * \~chinese \param index 为指定 Segment 的索引。
+ * \~chinese \param icon 为要设定的图标。
+ */
 void DSegmentedControl::setIcon(int index, const QIcon &icon)
 {
     at(index)->setIcon(icon);
