b000fc943864276add79e0850483e325583a4616
zccrs <zccrs@live.com>
2019-09-21 16:13:19 +0800

    Update translates

--- src/translations/dtkwidget_zh_CN.ts
+++ src/translations/dtkwidget_zh_CN.ts
@@ -1,18 +1,20 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="zh_CN" version="2.1">
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
 <context>
     <name>DAboutDialog</name>
     <message>
-        <location filename="../src/widgets/daboutdialog.cpp" line="191"/>
+        <location filename="../widgets/daboutdialog.cpp" line="191"/>
         <source>Acknowledgements</source>
         <translation>鸣谢</translation>
     </message>
     <message>
-        <location filename="../src/widgets/dapplication.cpp" line="712"/>
+        <location filename="../widgets/dapplication.cpp" line="1043"/>
         <source>Version: %1</source>
         <translation>版本：%1</translation>
     </message>
     <message>
-        <location filename="../src/widgets/dapplication.cpp" line="716"/>
+        <location filename="../widgets/dapplication.cpp" line="1047"/>
         <source>%1 is released under %2</source>
         <translation>%1遵循%2协议发布</translation>
     </message>
@@ -20,87 +22,87 @@
 <context>
     <name>DCrumbEdit</name>
     <message>
-        <location filename="../src/widgets/dcrumbedit.cpp" line="530"/>
+        <location filename="../widgets/dcrumbedit.cpp" line="755"/>
         <source>Black</source>
         <translation>黑色</translation>
     </message>
     <message>
-        <location filename="../src/widgets/dcrumbedit.cpp" line="533"/>
+        <location filename="../widgets/dcrumbedit.cpp" line="758"/>
         <source>White</source>
         <translation>白色</translation>
     </message>
     <message>
-        <location filename="../src/widgets/dcrumbedit.cpp" line="536"/>
+        <location filename="../widgets/dcrumbedit.cpp" line="761"/>
         <source>Dark Gray</source>
         <translation>深灰色</translation>
     </message>
     <message>
-        <location filename="../src/widgets/dcrumbedit.cpp" line="539"/>
+        <location filename="../widgets/dcrumbedit.cpp" line="764"/>
         <source>Gray</source>
         <translation>灰色</translation>
     </message>
     <message>
-        <location filename="../src/widgets/dcrumbedit.cpp" line="542"/>
+        <location filename="../widgets/dcrumbedit.cpp" line="767"/>
         <source>Light Gray</source>
         <translation>浅灰色</translation>
     </message>
     <message>
-        <location filename="../src/widgets/dcrumbedit.cpp" line="545"/>
+        <location filename="../widgets/dcrumbedit.cpp" line="770"/>
         <source>Red</source>
         <translation>红色</translation>
     </message>
     <message>
-        <location filename="../src/widgets/dcrumbedit.cpp" line="548"/>
+        <location filename="../widgets/dcrumbedit.cpp" line="773"/>
         <source>Green</source>
         <translation>绿色</translation>
     </message>
     <message>
-        <location filename="../src/widgets/dcrumbedit.cpp" line="551"/>
+        <location filename="../widgets/dcrumbedit.cpp" line="776"/>
         <source>Blue</source>
         <translation>蓝色</translation>
     </message>
     <message>
-        <location filename="../src/widgets/dcrumbedit.cpp" line="554"/>
+        <location filename="../widgets/dcrumbedit.cpp" line="779"/>
         <source>Cyan</source>
         <translation>青色</translation>
     </message>
     <message>
-        <location filename="../src/widgets/dcrumbedit.cpp" line="557"/>
+        <location filename="../widgets/dcrumbedit.cpp" line="782"/>
         <source>Magenta</source>
         <translation>洋红色</translation>
     </message>
     <message>
-        <location filename="../src/widgets/dcrumbedit.cpp" line="560"/>
+        <location filename="../widgets/dcrumbedit.cpp" line="785"/>
         <source>Yellow</source>
         <translation>黄色</translation>
     </message>
     <message>
-        <location filename="../src/widgets/dcrumbedit.cpp" line="563"/>
+        <location filename="../widgets/dcrumbedit.cpp" line="788"/>
         <source>Dark Red</source>
         <translation>深红色</translation>
     </message>
     <message>
-        <location filename="../src/widgets/dcrumbedit.cpp" line="566"/>
+        <location filename="../widgets/dcrumbedit.cpp" line="791"/>
         <source>Dark Green</source>
         <translation>深绿色</translation>
     </message>
     <message>
-        <location filename="../src/widgets/dcrumbedit.cpp" line="569"/>
+        <location filename="../widgets/dcrumbedit.cpp" line="794"/>
         <source>Dark Blue</source>
         <translation>深蓝色</translation>
     </message>
     <message>
-        <location filename="../src/widgets/dcrumbedit.cpp" line="572"/>
+        <location filename="../widgets/dcrumbedit.cpp" line="797"/>
         <source>Dark Cyan</source>
         <translation>深青色</translation>
     </message>
     <message>
-        <location filename="../src/widgets/dcrumbedit.cpp" line="575"/>
+        <location filename="../widgets/dcrumbedit.cpp" line="800"/>
         <source>Dark Magenta</source>
         <translation>深紫红色</translation>
     </message>
     <message>
-        <location filename="../src/widgets/dcrumbedit.cpp" line="578"/>
+        <location filename="../widgets/dcrumbedit.cpp" line="803"/>
         <source>Dark Yellow</source>
         <translation>深黄色</translation>
     </message>
@@ -108,25 +110,33 @@
 <context>
     <name>DInputDialog</name>
     <message>
-        <location filename="../src/widgets/dinputdialog.cpp" line="42"/>
+        <location filename="../widgets/dinputdialog.cpp" line="42"/>
         <source>Cancel</source>
         <translation>取消</translation>
     </message>
     <message>
-        <location filename="../src/widgets/dinputdialog.cpp" line="43"/>
+        <location filename="../widgets/dinputdialog.cpp" line="43"/>
         <source>Confirm</source>
         <translation>确定</translation>
     </message>
 </context>
 <context>
+    <name>DSearchEdit</name>
+    <message>
+        <location filename="../widgets/dsearchedit.cpp" line="98"/>
+        <source>Search</source>
+        <translation>搜索</translation>
+    </message>
+</context>
+<context>
     <name>DShortcutEdit</name>
     <message>
-        <location filename="../src/widgets/dshortcutedit.cpp" line="31"/>
+        <location filename="../widgets/dshortcutedit.cpp" line="31"/>
         <source>Please input a new shortcut</source>
         <translation>请输入新的快捷键</translation>
     </message>
     <message>
-        <location filename="../src/widgets/dshortcutedit.cpp" line="102"/>
+        <location filename="../widgets/dshortcutedit.cpp" line="102"/>
         <source>None</source>
         <translation>无</translation>
     </message>
@@ -134,12 +144,12 @@
 <context>
     <name>QObject</name>
     <message>
-        <location filename="../src/widgets/dsimplelistview.cpp" line="1235"/>
+        <location filename="../widgets/dsimplelistview.cpp" line="1331"/>
         <source>No search result</source>
         <translation>无搜索结果</translation>
     </message>
     <message>
-        <location filename="../src/widgets/private/settings/content.cpp" line="189"/>
+        <location filename="../widgets/private/settings/content.cpp" line="216"/>
         <source>Restore Defaults</source>
         <translation>恢复默认设置</translation>
     </message>
@@ -147,7 +157,7 @@
 <context>
     <name>ShortcutEdit</name>
     <message>
-        <location filename="../src/widgets/private/settings/shortcutedit.cpp" line="119"/>
+        <location filename="../widgets/private/settings/shortcutedit.cpp" line="118"/>
         <source>Please enter a new shortcut</source>
         <translation>请输入新的快捷键</translation>
     </message>
@@ -155,17 +165,37 @@
 <context>
     <name>TitleBarMenu</name>
     <message>
-        <location filename="../src/widgets/dtitlebar.cpp" line="449"/>
+        <location filename="../widgets/dtitlebar.cpp" line="481"/>
+        <source>Theme</source>
+        <translation>主题</translation>
+    </message>
+    <message>
+        <location filename="../widgets/dtitlebar.cpp" line="482"/>
+        <source>Light Theme</source>
+        <translation>浅色主题</translation>
+    </message>
+    <message>
+        <location filename="../widgets/dtitlebar.cpp" line="483"/>
+        <source>Dark Theme</source>
+        <translation>深色主题</translation>
+    </message>
+    <message>
+        <location filename="../widgets/dtitlebar.cpp" line="484"/>
+        <source>System Theme</source>
+        <translation>跟随系统主题</translation>
+    </message>
+    <message>
+        <location filename="../widgets/dtitlebar.cpp" line="507"/>
         <source>Help</source>
         <translation>帮助</translation>
     </message>
     <message>
-        <location filename="../src/widgets/dtitlebar.cpp" line="456"/>
+        <location filename="../widgets/dtitlebar.cpp" line="514"/>
         <source>About</source>
         <translation>关于</translation>
     </message>
     <message>
-        <location filename="../src/widgets/dtitlebar.cpp" line="463"/>
+        <location filename="../widgets/dtitlebar.cpp" line="521"/>
         <source>Exit</source>
         <translation>退出</translation>
     </message>
