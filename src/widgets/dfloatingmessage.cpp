ee5fbbd5d6ee694bd3c621b175abfb4abd8b0ff3
zhangwengeng <zhangwengeng_cm@deepin.com>
2019-10-26 09:32:51 +0800

    :feat: Write code comments that conform to the doxygen rules for the DFloatingMessage class

--- src/widgets/dfloatingmessage.cpp
+++ src/widgets/dfloatingmessage.cpp
@@ -78,6 +78,38 @@ void DFloatingMessagePrivate::init()
     }
 }
 
+/*!
+ * \~chinese \class DFloatingMessage
+ * \~chinese \brief 是一个浮动消息的类, 类似于 ios 的通知栏消息, 然后有每一个新的消息对象, 就添加到一个竖直布局里面,
+ * \~chinese 然后一个消息的其布局模式是: icon (图标) + Message (消息) + QWidget (存放如控件按钮,可选) + closeButton (关闭按钮,可选)
+ * \~chinese 多个消息均是以竖直布局存放在一个 DMessageManager (消息管理类) 里面的
+ *
+ * \~chinese \htmlonly
+ * <pre style="font-family: FreeMono, Consolas, Menlo, 'Noto Mono', 'Courier New', Courier, monospace;line-height: 100%;">
+ * =============================================================================================
+ * =                                                                                           =
+ * =        ==========       =======================         =========        ==========       =
+ * =        = icon   =       =        message      =         =QWidget=        =  quit  =       =
+ * =        ==========       =======================         =========        ==========       =
+ * =          (必选)                 (必选)                    (永驻:可选)       (永驻:可选)       =
+ * =============================================================================================
+ * </pre>
+ * \endhtmlonly
+ * \~chinese 消息通知一共有两种类型,临时停留几秒和手动清除的永驻消息
+ */
+
+/*!
+ *
+ * \~chinese \enum DFloatingMessage::MessageType
+ * \~chinese DFloatingMessage::MessageType 定义了 DFloatingMessage 通知类型
+ *
+ * \~chinese \var DFloatingMessage:MessageType DFloatingMessage::TransientType
+ * \~chinese 临时的消息
+ *
+ * \~chinese \var DDFloatingMessage:MessageType DFloatingMessage::ResidentType
+ * \~chinese 常驻的消息
+ */
+
 DFloatingMessage::DFloatingMessage(MessageType notifyType, QWidget *parent)
     : DFloatingWidget(*new DFloatingMessagePrivate(this), parent)
 {
@@ -89,6 +121,10 @@ DFloatingMessage::DFloatingMessage(MessageType notifyType, QWidget *parent)
     d->init();
 }
 
+/*!
+ * \~chinese \brief 设置控件图标 icon
+ * \~chinese \param[in] ico 是最终的效果图标
+ */
 void DFloatingMessage::setIcon(const QIcon &ico)
 {
     D_D(DFloatingMessage);
@@ -96,6 +132,10 @@ void DFloatingMessage::setIcon(const QIcon &ico)
     d->iconButton->setIcon(ico);
 }
 
+/*!
+ * \~chinese \brief 设置显示的文本消息(文字)
+ * \~chinese \param[in] str 消息文本的具体文字内容
+ */
 void DFloatingMessage::setMessage(const QString &str)
 {
     D_D(DFloatingMessage);
@@ -103,6 +143,10 @@ void DFloatingMessage::setMessage(const QString &str)
     d->labMessage->setText(str);
 }
 
+/*!
+ * \~chinese \brief 消息里面的,水平布局里面的第三个位置, 用来存放一些自定义控件, 比如一个确定按钮
+ * \~chinese \param[in] w 插入的具体控件, 若之前此处已经有其他的控件, 先删除挪出空位子, 在将 w 控件放到这个上面
+ */
 void DFloatingMessage::setWidget(QWidget *w)
 {
     D_D(DFloatingMessage);
@@ -114,6 +158,10 @@ void DFloatingMessage::setWidget(QWidget *w)
     d->hBoxLayout->insertWidget(2, d->content);
 }
 
+/*!
+ * \~chinese \brief 若是为临时消息,则设置时间(毫秒)后会自动消失
+ * \~chinese \param[in] msec 消息显示的毫秒数, 默认是4000毫秒
+ */
 void DFloatingMessage::setDuration(int msec)
 {
     D_D(DFloatingMessage);
@@ -124,6 +172,10 @@ void DFloatingMessage::setDuration(int msec)
     d->timer->setInterval(msec);
 }
 
+/*!
+ * \~chinese \brief 显示事件,里面设置计时器开始工作
+ * \~chinese \param[in] event 系统传过来的显示消息事件,没有感兴趣的消息需要处理,直接所有事件继续传递给下一个对象
+ */
 void DFloatingMessage::showEvent(QShowEvent *event)
 {
     D_D(DFloatingMessage);
