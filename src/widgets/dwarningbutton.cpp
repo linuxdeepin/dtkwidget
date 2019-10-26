0fb7d31a7ec918f23db514a2384c70be837d2520
zhangwengeng <zhangwengeng_cm@deepin.com>
2019-10-26 16:56:49 +0800

    feat: Write code comments that conform to the doxygen rules for the DWarningButton class

--- src/widgets/dwarningbutton.cpp
+++ src/widgets/dwarningbutton.cpp
@@ -6,10 +6,25 @@
 
 DWIDGET_BEGIN_NAMESPACE
 
+/*!
+ * \~chinese \class DWarningButton
+ * \~chinese \brief 警告按钮控件, 继承于 DPushButton 控件, 主要替换了文字和背景的颜色
+ * \~chinese \note 通常在执行一些比较危险的动作时候, 给出的一个提醒标识, 但是并不强制阻止; 如: 某些小程序需要较高的权限才能够运行; 删除文件不可恢复等
+ * \~chinese \see DPushButton
+ */
+
+/*!
+ * \~chinese \brief 构造函数
+ * \~chinese \param[in] parent 本控件的父对象
+ */
 DWarningButton::DWarningButton(QWidget *parent): DPushButton(parent)
 {
 }
 
+/*!
+ * \~chinese \brief 初始化风格属性
+ * \~chinese \param[in] option 用来初始化本控件的基础的风格属性
+ */
 void DWarningButton::initStyleOption(QStyleOptionButton *option) const
 {
     DPushButton::initStyleOption(option);
