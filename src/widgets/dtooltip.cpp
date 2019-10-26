433b038f8250dc435673d51a44d28f4de701dff1
wang <wangpeng_cm@deepin.com>
2019-10-26 16:13:26 +0800

    feat: 添加 DToolTip 注释

--- src/widgets/dtooltip.cpp
+++ src/widgets/dtooltip.cpp
@@ -9,6 +9,17 @@
 
 DWIDGET_BEGIN_NAMESPACE
 
+/*!
+ * \~chinese \class DToolTip
+ * \~chinese \brief DToolTip类似于 QToolTip 功能的类
+ * \~chinese \li DToolTip提供了将 DToolTip 显示在指定位置的函数
+ */
+
+/*!
+ * \~chinese \brief DToolTip的构造函数
+ * \~chinese \param text文本信息
+ * \~chinese \param completionClose提示消息消失后是否销毁
+ */
 DToolTip::DToolTip(const QString &text, bool completionClose)
     : DTipLabel(text)
 {
@@ -17,6 +28,10 @@ DToolTip::DToolTip(const QString &text, bool completionClose)
     }
 }
 
+/*!
+ * \~chinese \brief DToolTip::sizeHint
+ * \~chinese \sa QLabel::sizeHint()
+ */
 QSize DToolTip::sizeHint() const
 {
     int radius = DStyleHelper(style()).pixelMetric(DStyle::PM_FrameRadius);
@@ -39,6 +54,11 @@ bool DToolTip::event(QEvent *e)
     return QLabel::event(e);
 }
 
+/*!
+ * \chinese \brief DToolTip::show设置提示消息出现的位置
+ * \chinese \param pos提示消息位置
+ * \chinese \param duration提示消息存在时间，单位毫秒
+ */
 void DToolTip::show(const QPoint &pos, int duration)
 {
     QTimer::singleShot(duration, this, &DTipLabel::close);
