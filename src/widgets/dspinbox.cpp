139209393525f5e14665f60186a8dad22e3d7450
王鹏 <wangpeng_cm@deepin.com>
2020-05-14 10:30:53 +0800

    fix: bug 23387 spinbox样式异常

--- src/widgets/dspinbox.cpp
+++ src/widgets/dspinbox.cpp
@@ -34,6 +34,7 @@ void DSpinBoxPrivate::init()
     D_Q(DSpinBox);
 
     lineEdit = new DLineEdit(q);
+    lineEdit->setClearButtonEnabled(false);
     q->setLineEdit(lineEdit->lineEdit());
     q->connect(lineEdit, &DLineEdit::alertChanged, q, &DSpinBox::alertChanged);
 }
