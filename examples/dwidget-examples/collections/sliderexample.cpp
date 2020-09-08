b9c0ae8f6ab845bf56fc4ca39776b9a238af4ebc
wangpeng <wangpeng@uniontech.com>
2020-09-08 14:20:25 +0800

    feat: 添加DSliderExample示例
    添加DSliderExample示例
    
    Log: 添加DSliderExample示例
    Change-Id: I56dbc0336e7639c7df7c78c8c1ea2f6b2f23ea28
    Reviewed-on: http://gerrit.uniontech.com/c/dtkwidget/+/4012
    Reviewed-by: <mailman@uniontech.com>
    Reviewed-by: chenke <chenke@uniontech.com>
    Tested-by: <mailman@uniontech.com>

--- examples/dwidget-examples/collections/sliderexample.cpp
+++ examples/dwidget-examples/collections/sliderexample.cpp
@@ -29,7 +29,9 @@
 #include <DMessageBox>
 #include <DWarningButton>
 #include <DSuggestButton>
+#include <DSlider>
 #include <DFrame>
+#include <DLabel>
 
 #include "sliderexample.h"
 
@@ -44,6 +46,34 @@ SliderExampleWindow::SliderExampleWindow(QWidget *parent)
 DSliderExample::DSliderExample(QWidget *parent)
     : ExampleWindowInterface(parent)
 {
+    DSlider *vSlider = new DSlider(Qt::Vertical);
+    DSlider *hSlider = new DSlider(Qt::Horizontal);
+    DSlider *hCalibration = new DSlider(Qt::Horizontal);
+    DLabel *hLabel = new DLabel;
+    DLabel *vLabel = new DLabel;
+    QVBoxLayout *mainLayout = new QVBoxLayout;
+
+    vSlider->setMinimumHeight(283);
+    hSlider->setMinimumWidth(479);
+    hCalibration->setMinimumWidth(507);
+    hCalibration->setBelowTicks({"", "", "", "", "", "", ""});
+
+    hLabel->setScaledContents(true);
+    vLabel->setScaledContents(true);
+
+    hLabel->setFixedSize(550, 426);
+    vLabel->setFixedSize(550, 350);
+
+    hLabel->setPixmap(QPixmap(":/images/example/DSlider_1.png"));
+    vLabel->setPixmap(QPixmap(":/images/example/DSlider_2.png"));
+
+    setLayout(mainLayout);
+    mainLayout->setSpacing(10);
+    mainLayout->addWidget(vSlider, 0, Qt::AlignCenter);
+    mainLayout->addWidget(hSlider, 0, Qt::AlignCenter);
+    mainLayout->addWidget(hCalibration, 0, Qt::AlignCenter);
+    mainLayout->addWidget(hLabel, 0, Qt::AlignCenter);
+    mainLayout->addWidget(vLabel, 0, Qt::AlignCenter);
 }
 
 QString DSliderExample::getTitleName() const
@@ -53,10 +83,19 @@ QString DSliderExample::getTitleName() const
 
 QString DSliderExample::getDescriptionInfo() const
 {
-    return QString();
+    return "圆角矩形滑块可以随意拖动，起止点一\n"
+            "定是从左往右递增，滑块以左部分是活\n"
+            "动色显示。\n\n"
+            "尖角的滑块不可以像圆角矩形滑块那样\n"
+            "进行随意拖动，底下对应的有刻度，刻\n"
+            "度上产生吸附力，尖角也只能在几个刻\n"
+            "度值上调整，更多强调的是用户一个值\n"
+            "的取舍。\n "
+            "对应的刻度可能有刻度值显示，也可能\n"
+            "没有刻度值（界面上的）。";
 }
 
 int DSliderExample::getFixedHeight() const
 {
-    return 300;
+    return 1276;
 }
