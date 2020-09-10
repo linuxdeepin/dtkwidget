f23f03e93f63211e489a36d561a0ae766727415a
ck <kegechen@gmail.com>
2020-09-10 09:51:58 +0800

    feat: add scrollbar example
    添加dscrollbar的示例，由于scrollbar会自动隐藏，这里只是将图放到一个scrollarea中
    
    Log: 添加scrollbar的示例
    Change-Id: Ie077a2a0098384dcbca78bb508707936829a7f6d
    Reviewed-on: http://gerrit.uniontech.com/c/dtkwidget/+/4226
    Reviewed-by: chenke <chenke@uniontech.com>
    Reviewed-by: <mailman@uniontech.com>
    Tested-by: <mailman@uniontech.com>

--- examples/dwidget-examples/collections/scrollbarexample.cpp
+++ examples/dwidget-examples/collections/scrollbarexample.cpp
@@ -21,21 +21,13 @@
 
 #include <QHBoxLayout>
 #include <QVBoxLayout>
-#include <QMenu>
-#include <QScrollArea>
-#include <QDebug>
-
-#include <DDialog>
-#include <DStyle>
-#include <DPushButton>
-#include <DMessageBox>
-#include <DWarningButton>
-#include <DSuggestButton>
+#include <DScrollArea>
+#include <QImageReader>
 #include <DFrame>
-
 #include "scrollbarexample.h"
 
 DWIDGET_USE_NAMESPACE
+DCORE_USE_NAMESPACE
 
 ScrollBarExampleWindow::ScrollBarExampleWindow(QWidget *parent)
     : PageWindowInterface(parent)
@@ -46,6 +38,23 @@ ScrollBarExampleWindow::ScrollBarExampleWindow(QWidget *parent)
 DScrollBarExample::DScrollBarExample(QWidget *parent)
     : ExampleWindowInterface(parent)
 {
+    QVBoxLayout *mLayout = new QVBoxLayout(this);
+    QImageReader reader;
+    reader.setFileName(":/images/example/DScrollBar_1.png");
+    reader.setScaledSize(reader.size().scaled(480, 120, Qt::KeepAspectRatio));
+
+    QLabel *sbimg = new QLabel;
+    sbimg->setAlignment(Qt::AlignHCenter);
+    sbimg->setPixmap(QPixmap::fromImageReader(&reader));
+
+    DScrollArea *sa = new DScrollArea;
+    QLabel *image = new QLabel;
+    image->setPixmap(QPixmap(":/images/example/DScrollBar.png"));
+    sa->setWidget(image);
+
+    mLayout->addWidget(sbimg, 0, Qt::AlignTop | Qt::AlignHCenter);
+    mLayout->addSpacing(5);
+    mLayout->addWidget(sa);
 }
 
 QString DScrollBarExample::getTitleName() const
@@ -55,10 +64,10 @@ QString DScrollBarExample::getTitleName() const
 
 QString DScrollBarExample::getDescriptionInfo() const
 {
-    return QString();
+    return QStringLiteral("所有产生滚动的地方");
 }
 
 int DScrollBarExample::getFixedHeight() const
 {
-    return 300;
+    return 500;
 }
