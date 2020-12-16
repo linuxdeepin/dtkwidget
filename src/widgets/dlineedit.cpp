7cebaec9d62b0c7585e8873bb59a0c14508f6321
ck <kegechen@gmail.com>
2020-12-16 10:19:47 +0800

    fix: DLineEdit cannot edit with focus
    DLineEdit::setFocus后还是无法输入，包含的QLineEdit子对象没有获取焦点， 此处将QLineEdit设置焦点代理。
    
    Log: 修复DLineEdit设置焦点后无法输入文字的问题
    Bug:
    Change-Id: Icaa2ad03c8e4a9726303ad8fb32ec3551eb2c548

--- src/widgets/dlineedit.cpp
+++ src/widgets/dlineedit.cpp
@@ -589,6 +589,7 @@ void DLineEditPrivate::init()
 
     hLayout = new QHBoxLayout(q);
     lineEdit = new QLineEdit(q);
+    q->setFocusProxy(lineEdit); // fix DlineEdit setFocut but lineEdit can not edit(without focus rect)
 
     control = new DAlertControl(lineEdit, q);
     q->connect(control, &DAlertControl::alertChanged, q, &DLineEdit::alertChanged);
