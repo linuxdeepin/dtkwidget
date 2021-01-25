78565cd2784ec59e2999e6dfa0e77c57e6b5b3bd
chenbin <chenbin@uniontech.com>
2021-01-25 15:58:59 +0800

    fix: 修复DCrumbEdit编辑状态复制不生效问题
    之前的逻辑，DCrumbEdit在选中时未处理仍在编辑的状态。
    编辑的状态有两种：重复标签不会生成Crumb导致处于正在
    编辑、正在编辑时光标移到最后有文本没有生成Crumb。修
    改方式为：添加正在编辑文本的逻辑复制的逻辑，删除之前
    复制未选中编辑文本的逻辑。
    并添加此bug的单元测试案例。
    
    Log: 修复DCrumbEdit复制问题
    Bug: https://pms.uniontech.com/zentao/bug-view-61562.html
    Change-Id: I6b847c17bfcb6890097cf9e020965f04ca85780f

---
+++ tests/src/widgets/ut_dcrumbedit.cpp
@@ -0,0 +1,94 @@
+#include <gtest/gtest.h>
+#include <QTest>
+#include <QClipboard>
+#include <QMimeData>
+
+#include "dcrumbedit.h"
+#include <QDebug>
+
+DWIDGET_USE_NAMESPACE
+
+class ut_DCrumbedit : public testing::Test
+{
+protected:
+    void SetUp() override;
+    void TearDown() override;
+
+    DCrumbEdit *edit;
+};
+
+void ut_DCrumbedit::SetUp()
+{
+    edit = new DCrumbEdit;
+}
+
+void ut_DCrumbedit::TearDown()
+{
+    delete edit;
+}
+
+TEST_F(ut_DCrumbedit, createMimeDataFromSelection)
+{
+    edit->appendCrumb("人物");
+    edit->appendCrumb("儿童");
+    edit->appendCrumb("照片");
+
+    QTextCursor cursor = edit->textCursor();
+    cursor.setPosition(0);
+    cursor.insertText("测试1");
+    cursor.movePosition(QTextCursor::Right);
+    cursor.insertText("测试2");
+    cursor.movePosition(QTextCursor::Right);
+    cursor.insertText("测试3");
+    cursor.movePosition(QTextCursor::End);
+    cursor.insertText("测试代码调试添加GTest");
+
+    // 重置光标到最前面
+    cursor.setPosition(0);
+    // 此时 测试1 人物 测试2 儿童 测试3 照片 测试代码调试添加GTest
+    edit->setTextCursor(cursor);
+
+    ASSERT_EQ(edit->textCursor().position(), 0);
+
+    // 模拟鼠标中选中操作 选中字符为： 测试1
+    QTest::keyClick(edit, Qt::Key_Right, Qt::ShiftModifier);
+    QTest::keyClick(edit, Qt::Key_Right, Qt::ShiftModifier);
+    QTest::keyClick(edit, Qt::Key_Right, Qt::ShiftModifier);
+    // 此时位置： 测试1 | 人物 测试2 儿童 测试3 照片 测试代码调试添加GTest
+    ASSERT_TRUE(edit->textCursor().hasSelection());
+    ASSERT_FALSE(edit->textCursor().selectedText().isEmpty());
+    edit->copy();
+    ASSERT_EQ(edit->createMimeDataFromSelection()->text(), "测试1");
+    ASSERT_EQ(qApp->clipboard()->text(), "测试1");
+
+    // 模拟鼠标中选中操作 选中字符为： 测试1 人物
+    QTest::keyClick(edit, Qt::Key_Right, Qt::ShiftModifier);
+    // 此时位置 测试1 人物  | 测试2 儿童 测试3 照片 测试代码调试添加GTest
+    ASSERT_TRUE(edit->textCursor().hasSelection());
+    ASSERT_FALSE(edit->textCursor().selectedText().isEmpty());
+    edit->copy();
+    ASSERT_EQ(edit->createMimeDataFromSelection()->text(), "测试1 人物");
+    ASSERT_EQ(qApp->clipboard()->text(), "测试1 人物");
+
+    edit->moveCursor(QTextCursor::Start);
+    edit->moveCursor(QTextCursor::Right);
+    edit->moveCursor(QTextCursor::Right);
+    edit->moveCursor(QTextCursor::Right);
+
+    // 选中字符为： 人物 测试
+    QTest::keyClick(edit, Qt::Key_Right, Qt::ShiftModifier);
+    QTest::keyClick(edit, Qt::Key_Right, Qt::ShiftModifier);
+    QTest::keyClick(edit, Qt::Key_Right, Qt::ShiftModifier);
+    // 此时位置 测试1 人物 测试|2 儿童 测试3 照片 测试代码调试添加GTest
+    edit->copy();
+    ASSERT_EQ(edit->createMimeDataFromSelection()->text(), "人物 测试");
+    ASSERT_EQ(qApp->clipboard()->text(), "人物 测试");
+
+    edit->moveCursor(QTextCursor::Start);
+
+    // 选中所有字符
+    QTest::keyClick(edit, Qt::Key_End, Qt::ShiftModifier);
+    edit->copy();
+    ASSERT_EQ(edit->createMimeDataFromSelection()->text(), "测试1 人物 测试2 儿童 测试3 照片 测试代码调试添加GTest");
+    ASSERT_EQ(qApp->clipboard()->text(), "测试1 人物 测试2 儿童 测试3 照片 测试代码调试添加GTest");
+}
