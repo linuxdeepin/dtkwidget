06f0fa3dd116ea24e61a165f1e8f5b070ffd228e
wangpenga <wangpenga@uniontech.com>
2021-07-01 15:17:37 +0800

    fix: 单元测试内存泄露检测并修复
    添加内存检测参数，修复检测到的内存泄露
    
    Log:
    Change-Id: Ibd472f87b083041f6765461e7f6f59ede91210bb

--- tests/test-recoverage.sh
+++ tests/test-recoverage.sh
@@ -3,18 +3,27 @@
 BUILD_DIR=build
 REPORT_DIR=report
 
-cd ../
+#cd ../
+#rm -rf $BUILD_DIR
+#mkdir $BUILD_DIR
+#cd $BUILD_DIR
+#qmake .. CONFIG+=debug
+#make -j$(nproc)
+#cd ../tests/
+
 rm -rf $BUILD_DIR
 mkdir $BUILD_DIR
 cd $BUILD_DIR
-qmake ..
-make -j16
-cd tests/
-make check
+qmake ../ CONFIG+=debug
+export ASAN_OPTIONS=halt_on_error=0
+make check -j$(nproc)
 
 lcov -d ./ -c -o coverage_all.info
 lcov --extract coverage_all.info $EXTRACT_ARGS --output-file coverage.info
 lcov --remove coverage_all.info "*/tests/*" "*/usr/include*" "*build/src*" --output-file coverage.info
 genhtml -o ../../tests/$REPORT_DIR coverage.info
 
-rm -rf ../../$BUILD_DIR
+cd ..
+genhtml -o $REPORT_DIR $BUILD_DIR/coverage.info
+
+test -e ./build/asan.log* && mv ./build/asan.log* ./build/asan_dtkwidget.log || echo "Not detected any memory leak."
