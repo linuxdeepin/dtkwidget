19cad4f332e9105e67995580589fb24df6ecea9f
wangpenga <wangpenga@uniontech.com>
2021-07-14 10:43:47 +0800

    fix: 单元测试不能生成config文件编译失败
    在单元测试中脚本中的未生成dtkwidget_config.h，编译失败。
    将单元测试脚本的最新修改同步进来，添加了参数生成测试数据。
    
    Log:
    Change-Id: I62d304d138873bfd43dd50db0df4a24d1cf80cf6
    (cherry picked from commit 79e87ab79bf9ce8dcae0ba6fb911685c9418d264)

--- tests/test-recoverage.sh
+++ tests/test-recoverage.sh
@@ -14,16 +14,21 @@ REPORT_DIR=report
 rm -rf $BUILD_DIR
 mkdir $BUILD_DIR
 cd $BUILD_DIR
+
+qmake .. CONFIG+=debug && make qmake_all
+#make -j$(nproc)
+cd ../tests/
+
+rm -rf $BUILD_DIR
+mkdir $BUILD_DIR
+cd $BUILD_DIR
 qmake ../ CONFIG+=debug
 export ASAN_OPTIONS=halt_on_error=0
-make check -j$(nproc)
+TESTARGS="--gtest_output=xml:dde_test_report_dtkwidget.xml" make check -j$(nproc)
 
 lcov -d ./ -c -o coverage_all.info
 lcov --extract coverage_all.info $EXTRACT_ARGS --output-file coverage.info
 lcov --remove coverage_all.info "*/tests/*" "*/usr/include*" "*build/src*" --output-file coverage.info
 genhtml -o ../../tests/$REPORT_DIR coverage.info
-
-cd ..
-genhtml -o $REPORT_DIR $BUILD_DIR/coverage.info
 
 test -e ./build/asan.log* && mv ./build/asan.log* ./build/asan_dtkwidget.log || echo "Not detected any memory leak."
