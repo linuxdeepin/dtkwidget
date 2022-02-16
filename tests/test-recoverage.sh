2e5fb3280cee7296fc894b60c336b06a3c7579df
AlexOne <wangpenga@uniontech.com>
2022-02-16 14:24:12 +0800

    chore: 修改单元测试生成目录
    修改单元测试相关文件的目录结构
    
    Log:
    Influence: 单元测试目录结构
    Change-Id: I4685e03a884eec2c876fc483748f9b169e0d4d40

--- tests/test-recoverage.sh
+++ tests/test-recoverage.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-BUILD_DIR=build
-REPORT_DIR=report
+BUILD_DIR=`pwd`/../build-ut
+HTML_DIR=${BUILD_DIR}/html
+XML_DIR=${BUILD_DIR}/report
 
 cd ../
 #rm -rf $BUILD_DIR
@@ -24,7 +25,7 @@ mkdir $BUILD_DIR
 cd $BUILD_DIR
 qmake ../ CONFIG+=debug
 export ASAN_OPTIONS=halt_on_error=0
-TESTARGS="--gtest_output=xml:dde_test_report_dtkwidget.xml" make check -j$(nproc)
+TESTARGS="--gtest_output=xml:${XML_DIR}/dde_test_report_dtkwidget.xml" make check -j$(nproc)
 
 lcov -d ./ -c -o coverage_all.info
 lcov --extract coverage_all.info $EXTRACT_ARGS --output-file coverage.info
@@ -54,8 +55,7 @@ filter_files=(
 "*/dbusinterface.*"
 "*/ddesktopservices_linux.cpp"
 )
-lcov --remove coverage_all.info "*/tests/*" "*/usr/include*" "*build/src*" ${filter_files[*]} --output-file coverage.info
-genhtml -o ../../tests/$REPORT_DIR coverage.info
+lcov --remove coverage_all.info "*/tests/*" "*/usr/include*" "*build-ut/src*" ${filter_files[*]} --output-file coverage.info
+genhtml -o $HTML_DIR $BUILD_DIR/coverage.info
 
-cd ..
-test -e ./build/asan.log* && mv ./build/asan.log* ./build/asan_dtkwidget.log || touch ./build/asan.log
+test -e ${BUILD_DIR}/asan.log* && mv ${BUILD_DIR}/asan.log* ${BUILD_DIR}/asan_dtkwidget.log || touch ${BUILD_DIR}/asan_dtkwidget.log
