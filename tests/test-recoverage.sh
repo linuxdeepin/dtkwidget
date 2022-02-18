3fceb6325c646379be12b826a25a6de687f14d8a
AlexOne <wangpenga@uniontech.com>
2022-02-18 17:38:44 +0800

    chore: 按照要求修改单元测试文件名
    按照要求修改单元测试文件名
    
    Log:
    Influence: none
    Change-Id: I2804c65f306111397f43c1c780d19ca23e1cbdbc

--- tests/test-recoverage.sh
+++ tests/test-recoverage.sh
@@ -25,7 +25,7 @@ mkdir $BUILD_DIR
 cd $BUILD_DIR
 qmake ../ CONFIG+=debug
 export ASAN_OPTIONS=halt_on_error=0
-TESTARGS="--gtest_output=xml:${XML_DIR}/dde_test_report_dtkwidget.xml" make check -j$(nproc)
+TESTARGS="--gtest_output=xml:${XML_DIR}/report_dtkwidget.xml" make check -j$(nproc)
 
 lcov -d ./ -c -o coverage_all.info
 lcov --extract coverage_all.info $EXTRACT_ARGS --output-file coverage.info
@@ -56,6 +56,6 @@ filter_files=(
 "*/ddesktopservices_linux.cpp"
 )
 lcov --remove coverage_all.info "*/tests/*" "*/usr/include*" "*build-ut/src*" ${filter_files[*]} --output-file coverage.info
-genhtml -o $HTML_DIR $BUILD_DIR/coverage.info
+genhtml -o $HTML_DIR $BUILD_DIR/coverage.info && mv ${BUILD_DIR}/html/index.html ${BUILD_DIR}/html/cov_dtkwidget.html
 
 test -e ${BUILD_DIR}/asan.log* && mv ${BUILD_DIR}/asan.log* ${BUILD_DIR}/asan_dtkwidget.log || touch ${BUILD_DIR}/asan_dtkwidget.log
