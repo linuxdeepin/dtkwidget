44f2c5c251b3570a5ad60448d50976106e5f94b3
Chen Bin <chenbin@uniontech.com>
2022-05-07 21:10:50 +0800

    feat(打印预览): 增加插件控制打印属性
    1. 新增插件类控制打印属性的状态（显示、禁用和隐藏）以及默认属性
    2. 修改打印对话框的源代码适配插件模式
    3. 新增测试用例，测试上述场景。
    
    Log:
    Change-Id: I7af2de238070263be92d1b84f0f4ac136c1b1625

---
+++ src/widgets/dprintpreviewsettinginfo.cpp
@@ -0,0 +1,16 @@
+#include "dprintpreviewsettinginfo.h"
+
+DWIDGET_BEGIN_NAMESPACE
+
+DPrintPreviewSettingInfo::DPrintPreviewSettingInfo(SettingType type)
+    : t(type)
+{
+
+}
+
+Dtk::Widget::DPrintPreviewSettingInfo::~DPrintPreviewSettingInfo()
+{
+
+}
+
+DWIDGET_END_NAMESPACE
