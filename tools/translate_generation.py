8d8912b04f6216d674c8e1c7bf5d14b577cf15f7
xzl <1037981455@qq.com>
2022-08-11 09:32:48 +0800

    chore: adapt REUSE license header
    修改为开源合规
    
    Log: 修改项目合规

--- tools/translate_generation.py
+++ tools/translate_generation.py
@@ -1,5 +1,9 @@
 #!env python
 
+# SPDX-FileCopyrightText: 2022 UnionTech Software Technology Co., Ltd.
+#
+# SPDX-License-Identifier: LGPL-3.0-or-later
+
 import sys,os,fnmatch
 from subprocess import call
 
