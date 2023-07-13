c0954d82bedee1a478cdbfd649bf3d447e17937c
LyYeeeYuwu <1066454892@qq.com>
2023-07-13 10:53:55 +0800

    doc: update docs for dprogressbar
    更新dprogressbar的文档
    
    Log: update docs
    
    Issue: https://github.com/linuxdeepin/dtk/issues/94

--- src/widgets/dprogressbar.cpp
+++ src/widgets/dprogressbar.cpp
@@ -8,13 +8,30 @@
 #include <QStyleOptionProgressBar>
 
 DWIDGET_BEGIN_NAMESPACE
+/*!
+@~english
+  @class Dtk::Widget::DProgressBar
+  @brief DProgressBar is a widget for progress bar
+  @details The DProgressBar Class inherits from The QProgressBar Class and provides the function of displaying progress bar.
+*/
 
+/*!
+@~english
+  @brief DProgressBar constructor.
+
+  @param[in] parent is passed to QProgressBar constructor.
+*/
 DProgressBar::DProgressBar(QWidget *parent)
     : QProgressBar(parent)
 {
 
 }
 
+/*!
+@~english
+  @brief similar to QProgressBar::sizeHint()，return the siezehint of progress bar
+  @return sizehint of the progress bar
+*/
 QSize DProgressBar::sizeHint() const
 {
     if (isTextVisible()) {
@@ -26,6 +43,11 @@ QSize DProgressBar::sizeHint() const
     return style()->sizeFromContents(QStyle::CT_ProgressBar, &opt, QSize(1, 1), this);
 }
 
+/*!
+@~english
+  @brief similar to QProgressBar::minimumSizeHint()，return the minimum siezehint of progress bar
+  @return minimum sizehint of the progress bar
+*/
 QSize DProgressBar::minimumSizeHint() const
 {
     if (isTextVisible()) {
