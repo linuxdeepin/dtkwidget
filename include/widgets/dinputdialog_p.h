bda41420b70da2d41fa1c61db1f16180b00c4026
xzl <xiangzelong@deepin.org>
2023-07-28 10:36:01 +0800

    chore: remove useless files
    Log：

--- include/widgets/dinputdialog_p.h
+++
@@ -1,9 +0,0 @@
-// SPDX-FileCopyrightText: 2015 - 2022 UnionTech Software Technology Co., Ltd.
-//
-// SPDX-License-Identifier: LGPL-3.0-or-later
-
-#ifndef DINPUTDIALOG_P_H
-#define DINPUTDIALOG_P_H
-
-#endif // DINPUTDIALOG_P_H
-
