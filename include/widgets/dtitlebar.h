3a2f266613a358a86e89e6f1cd383f58538a68a4
Felix Yan <felixonmars@archlinux.org>
2023-09-30 20:53:32 +0300

    chore: correct typos about action slots
    Log:

--- include/widgets/dtitlebar.h
+++ include/widgets/dtitlebar.h
@@ -123,11 +123,11 @@ private:
 #ifndef QT_NO_MENU
     D_PRIVATE_SLOT(void _q_addDefaultMenuItems())
     D_PRIVATE_SLOT(void _q_helpActionTriggered())
-    D_PRIVATE_SLOT(void _q_feedbackActionTriggerd())
+    D_PRIVATE_SLOT(void _q_feedbackActionTriggered())
     D_PRIVATE_SLOT(void _q_aboutActionTriggered())
     D_PRIVATE_SLOT(void _q_quitActionTriggered())
     D_PRIVATE_SLOT(void _q_switchThemeActionTriggered(QAction*))
-    D_PRIVATE_SLOT(void _q_toolBarActionTriggerd())
+    D_PRIVATE_SLOT(void _q_toolBarActionTriggered())
 #endif
 };
 
