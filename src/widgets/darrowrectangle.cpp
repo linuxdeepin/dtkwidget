af347e40fbd44ca00f2665fbd1a1194339ac14b4
zhangkun <zhangkun2@uniontech.com>
2023-12-13 20:40:28 +0800

    fix: 修复DArrowRectangle在屏幕边缘处移动时其箭头有残影
    根因: 给窗口设置mask的时候不会自动重绘，之前的绘制的残留部分没有消掉
    解决: 在设置mask前触发重绘
    
    Log: 修复DArrowRectangle在屏幕边缘处移动时其箭头有残影
    Bug: https://pms.uniontech.com/bug-view-230623.html
    Influence: DArrowRectangle在屏幕边缘不会有残影
    Change-Id: I33199747d0a634a70c5101e2fdba5d1db3e45a4d

--- src/widgets/darrowrectangle.cpp
+++ src/widgets/darrowrectangle.cpp
@@ -1169,6 +1169,7 @@ void DArrowRectanglePrivate::updateClipPath()
         QPolygon polygon = outPath.united(path).toFillPolygon().toPolygon();
 
         q->clearMask();
+        q->repaint();
         q->setMask(polygon);
         if (m_blurBackground)
             m_blurBackground->setMaskPath(path);
