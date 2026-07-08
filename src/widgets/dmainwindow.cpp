a183e23bff84304dc19f9e5d9907fd919985ead2
liujianqiang <liujianqiang@uniontech.com>
2026-07-08 15:52:37 +0800

    chore(copyright): update SPDX copyright year to 2026
    Update the SPDX-FileCopyrightText header in dmainwindow.cpp
    to extend the copyright year range from 2017-2023 to 2017-2026.
    
    Log: Updated SPDX copyright year range to 2017-2026 in dmainwindow.cpp
    
    Influence:
    1. Verify SPDX header in dmainwindow.cpp reflects the correct year range
    
    chore(copyright): 更新 SPDX 版权年份至 2026
    
    更新 dmainwindow.cpp 中的 SPDX-FileCopyrightText 头部，
    将版权年份范围从 2017-2023 扩展为 2017-2026。
    
    Log: 更新 dmainwindow.cpp 中 SPDX 版权年份范围为 2017-2026
    
    Influence:
    1. 验证 dmainwindow.cpp 的 SPDX 头部年份范围正确

--- src/widgets/dmainwindow.cpp
+++ src/widgets/dmainwindow.cpp
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2017 - 2023 UnionTech Software Technology Co., Ltd.
+// SPDX-FileCopyrightText: 2017 - 2026 UnionTech Software Technology Co., Ltd.
 //
 // SPDX-License-Identifier: LGPL-3.0-or-later
 
